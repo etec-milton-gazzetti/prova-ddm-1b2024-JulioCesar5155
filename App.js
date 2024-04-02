c4909b0b5ba581a9e4be95f280476aa3db9c0da1
JulioCesar5155 <125457479+JulioCesar5155@users.noreply.github.com>
2024-04-02 12:52:55 +0000

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import { NavigationContainer, getStateFromPath } from '@react-navigation/native';
 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>História</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('About')} style={{backgroundColor: 'blue', color: 'white', padding: 14, borderRadius: 20 }}>
+        <Text style={{color: 'white',}}>Ir para Sobre</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function AboutScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sobre mim</Text>
+      <TouchableOpacity  onPress={() => navigation.goBack()}>
+        <Text>Voltar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="About" component={AboutScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
